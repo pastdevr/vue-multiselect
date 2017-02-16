2d237c1e0df7b1ab65a74d37e53e93635856437a
pastdevr <programmer-autodidact@hotmail.com>
2017-02-16 15:30:56 +0300

    Do not remove whitespace from the right end of a string.

--- src/multiselectMixin.js
+++ src/multiselectMixin.js
@@ -351,7 +351,7 @@ module.exports = {
       )(options)
     },
     updateSearch (query) {
-      this.search = query.trim().toString()
+      this.search = query.trimLeft().toString()
     },
     /**
      * Finds out if the given query is already present
